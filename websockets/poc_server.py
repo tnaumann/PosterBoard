7f5d834b00a754adafa1952cfaa3e979f8cb977b
Tristan Naumann <tristan.naumann@gmail.com>
2012-05-07 00:49:43 -0400

    Adding code for Phidget

--- websockets/poc_server.py
+++ websockets/poc_server.py
@@ -3,11 +3,20 @@
 import base64
 import hashlib
 import mimetools
+import Queue
 import socket
 import StringIO
 import struct
 import threading
 import time
+
+#Basic imports
+from ctypes import *
+import sys
+#Phidget specific imports
+from Phidgets.PhidgetException import PhidgetErrorCodes, PhidgetException
+from Phidgets.Events.Events import AttachEventArgs, DetachEventArgs, ErrorEventArgs, OutputChangeEventArgs, TagEventArgs
+from Phidgets.Devices.RFID import RFID
 
 SERVER_HANDSHAKE_HYBI = """
 HTTP/1.1 101 Switching Protocols\r
@@ -18,40 +27,164 @@ Sec-WebSocket-Accept: %s\r
 
 GUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
 
+#Create an RFID object
+try:
+    rfid = RFID()
+except RuntimeError as e:
+    print("Runtime Exception: %s" % e.details)
+    print("Exiting....")
+    exit(1)
+
 def encode(buf):
-  header = 0x81
-  header = struct.pack('>BB', header, len(buf))
-  return header + buf
-  
+	header = 0x81
+	header = struct.pack('>BB', header, len(buf))
+	return header + buf
 
+cur_s = None
 def handle(s):
-  request_text = s.recv(4096)
-  print repr(request_text)
-  request_line, headers_alone = request_text.split('\r\n', 1)
-  headers = mimetools.Message(StringIO.StringIO(headers_alone))
+	cur_s = s
+    
+	request_text = s.recv(4096)
+	print repr(request_text)
+	request_line, headers_alone = request_text.split('\r\n', 1)
+	headers = mimetools.Message(StringIO.StringIO(headers_alone))
 
-  accept = base64.b64encode(
-    hashlib.sha1(headers['Sec-WebSocket-Key'] + GUID).digest())
-  response = SERVER_HANDSHAKE_HYBI % (accept)
-  response = response.strip() + '\r\n\r\n'
+	accept = base64.b64encode(
+		hashlib.sha1(headers['Sec-WebSocket-Key'] + GUID).digest())
+	response = SERVER_HANDSHAKE_HYBI % (accept)
+	response = response.strip() + '\r\n\r\n'
 
-  s.send(response)
-  time.sleep(1)
-  s.send(encode('hello'))
-  time.sleep(1)
-  s.send(encode('world'))
-  time.sleep(1)
-  s.close()
+	s.send(response)
+	
+	time.sleep(1)
+	s.send(encode("hello %s" % ("there")))
+	time.sleep(1)
+	s.send(encode('world'))
+	time.sleep(1)
+	
+	print("Press Enter to quit....")
 
-print "Starting socket server...",
+	chr = sys.stdin.read(1)
+
+	try:
+		lastTag = rfid.getLastTag()
+		print("Last Tag: %s" % (lastTag))
+	except PhidgetException as e:
+		print("Phidget Exception %i: %s" % (e.code, e.details))
+
+	print("Closing...")
+
+	try:
+		rfid.closePhidget()
+	except PhidgetException as e:
+		print("Phidget Exception %i: %s" % (e.code, e.details))
+		print("Exiting....")
+		exit(1)
+
+	print("Done.")
+	exit(0)
+		
+	s.close()
+	
+print "Initializing socket server...",
 s = socket.socket()
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(('', 9876))
 s.listen(1)
 print "Done."
+
+
+
+#Information Display Function
+def displayDeviceInfo():
+    print("|------------|----------------------------------|--------------|------------|")
+    print("|- Attached -|-              Type              -|- Serial No. -|-  Version -|")
+    print("|------------|----------------------------------|--------------|------------|")
+    print("|- %8s -|- %30s -|- %10d -|- %8d -|" % (rfid.isAttached(), rfid.getDeviceName(), rfid.getSerialNum(), rfid.getDeviceVersion()))
+    print("|------------|----------------------------------|--------------|------------|")
+    print("Number of outputs: %i -- Antenna Status: %s -- Onboard LED Status: %s" % (rfid.getOutputCount(), rfid.getAntennaOn(), rfid.getLEDOn()))
+
+#Event Handler Callback Functions
+def rfidAttached(e):
+    attached = e.device
+    print("RFID %i Attached!" % (attached.getSerialNum()))
+
+def rfidDetached(e):
+    detached = e.device
+    print("RFID %i Detached!" % (detached.getSerialNum()))
+
+def rfidError(e):
+    try:
+        source = e.device
+        print("RFID %i: Phidget Error %i: %s" % (source.getSerialNum(), e.eCode, e.description))
+    except PhidgetException as e:
+        print("Phidget Exception %i: %s" % (e.code, e.details))
+
+def rfidOutputChanged(e):
+    source = e.device
+    print("RFID %i: Output %i State: %s" % (source.getSerialNum(), e.index, e.state))
+
+def rfidTagGained(e):
+    source = e.device
+    rfid.setLEDOn(1)
+    print("RFID %i: Tag Read: %s" % (source.getSerialNum(), e.tag))
+    if cur_s:
+        cur_s.send(encode("RFID %i: Tag Read: %s" % (source.getSerialNum(), e.tag)))
+
+def rfidTagLost(e):
+    source = e.device
+    rfid.setLEDOn(0)
+    print("RFID %i: Tag Lost: %s" % (source.getSerialNum(), e.tag))
+    if cur_s:
+        cur_s.send(encode("RFID %i: Tag Lost: %s" % (source.getSerialNum(), e.tag)))
+
+#Main Program Code
+try:
+    rfid.setOnAttachHandler(rfidAttached)
+    rfid.setOnDetachHandler(rfidDetached)
+    rfid.setOnErrorhandler(rfidError)
+    rfid.setOnOutputChangeHandler(rfidOutputChanged)
+    rfid.setOnTagHandler(rfidTagGained)
+    rfid.setOnTagLostHandler(rfidTagLost)
+except PhidgetException as e:
+    print("Phidget Exception %i: %s" % (e.code, e.details))
+    print("Exiting....")
+    exit(1)
+
+print("Opening phidget object....")
+
+try:
+    rfid.openPhidget()
+except PhidgetException as e:
+    print("Phidget Exception %i: %s" % (e.code, e.details))
+    print("Exiting....")
+    exit(1)
+
+print("Waiting for attach....")
+
+try:
+    rfid.waitForAttach(10000)
+except PhidgetException as e:
+    print("Phidget Exception %i: %s" % (e.code, e.details))
+    try:
+        rfid.closePhidget()
+    except PhidgetException as e:
+        print("Phidget Exception %i: %s" % (e.code, e.details))
+        print("Exiting....")
+        exit(1)
+    print("Exiting....")
+    exit(1)
+else:
+    displayDeviceInfo()
+
+print("Turning on the RFID antenna....")
+rfid.setAntennaOn(True)
+
+print "Running socket server...",
 while 1:
-  t,_ = s.accept()
-  threading.Thread(target = handle, args = (t,)).start()
+	t,_ = s.accept()
+	threading.Thread(target = handle, args = (t,)).start()
+print "Done."
 
 
 
