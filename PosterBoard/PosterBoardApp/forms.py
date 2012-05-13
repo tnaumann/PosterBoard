41b894e0c05d5f61593fa94502bdfe1598cc672b
MV <mvartak03@gmail.com>
2012-05-13 16:15:46 -0400

    Form update

--- PosterBoard/PosterBoardApp/forms.py
+++ PosterBoard/PosterBoardApp/forms.py
@@ -1,13 +1,14 @@
 from django import forms
 
 class PosterForm(forms.Form):
-    posterFile1 = forms.FileField(
-        label='Select poster file',
-        help_text='max 2 MB'
-    )
+    filename = forms.CharField(max_length=200)
     event_date = forms.DateTimeField()
     start_time = forms.TimeField()
     end_time = forms.TimeField()
     tag1 = forms.CharField(max_length=200)
-    email = forms.EmailField()
-    title = forms.CharField(max_length=200)
+    tag2 = forms.CharField(max_length=200, required=False)
+    tag3 = forms.CharField(max_length=200, required=False)
+    tag4 = forms.CharField(max_length=200, required=False)
+    tag5 = forms.CharField(max_length=200, required=False)
+    email = forms.EmailField(initial='')
+    title = forms.CharField(max_length=200, required=False)
